8fef21689792d0c9073f53b22d21ee6e0b43cfb0
Marc Mance <marc@jamesf.iin.private>
2013-04-05 11:56:19 -0400

    desktop

--- changeViewport.js
+++ changeViewport.js
@@ -1,9 +1,13 @@
 
 viewport = document.querySelector("meta[name=viewport]");
-if(screen.width > 320) {
-	alert('ipad');
+if(screen.width <=  320){
+	viewport.setAttribute('content', 'width=640');
+}
+else if(screen.width > 320 && screen.width <= 1024) {
+	//alert('ipad');
 	viewport.setAttribute('content', 'width=1024');
 }
 else {
-	viewport.setAttribute('content', 'width=640');
+	//alert('here');
+	viewport.setAttribute('content', 'width=device-width');
 }
